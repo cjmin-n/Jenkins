ea35e496f67054caafe39b530759cb365789eca8
jeongmin <mymemose@naver.com>
2024-12-04 11:14:41 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/jenkins_test_app/menu/repository/MenuRepository.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.jenkins_test_app.menu.repository;
+
+import com.ohgiraffers.jenkins_test_app.menu.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Integer> {
+
+
+
+}
